7cc1d0facd32024540ed39ecaed8813016e8a6a3
Gary Rowe <g.rowe@froot.co.uk>
2013-07-21 08:22:13 +0100

    Removed merge artifacts from assets/views that appeared as top level directories and HTML files Updated site documentation to reflect current situation

--- js/mac_link.js
+++
@@ -1,1 +0,0 @@
-document.write("<a href=\"releases/multibit-0.5.9/multibit-0.5.9.dmg\">Mac OS X DMG file</a>&nbsp;&nbsp;(9.4MB, <a href=\"releases/multibit-0.5.9/multibit-0.5.9.dmg.asc\">signature</a>)")
