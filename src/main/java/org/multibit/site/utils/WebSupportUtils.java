7b951b60e08404c32ec1b270ab83ffd62518e137
Gary Rowe <g.rowe@froot.co.uk>
2015-02-10 21:17:13 +0000

    #168 Switched it off properly

--- src/main/java/org/multibit/site/utils/WebSupportUtils.java
+++ src/main/java/org/multibit/site/utils/WebSupportUtils.java
@@ -2,11 +2,8 @@ package org.multibit.site.utils;
 
 import org.multibit.site.core.atom.AtomFeed;
 import org.multibit.site.core.atom.AtomFeedBuilder;
-import org.multibit.site.core.resizer.ScreenShotResizer;
 import org.multibit.site.core.sitemap.SiteMap;
 import org.multibit.site.core.sitemap.SiteMapBuilder;
-
-import java.nio.file.Paths;
 
 /**
  * <p>Utility to provide the following to developers:</p>
@@ -30,7 +27,7 @@ public class WebSupportUtils {
 
     // At present this results in slight fuzziness
     // so has been switched off
-    ScreenShotResizer.resizeAll(Paths.get("src/main/resources/assets/images/en/screenshots/mbhd-0.1"), "*.png");
+    //ScreenShotResizer.resizeAll(Paths.get("src/main/resources/assets/images/en/screenshots/mbhd-0.1"), "*.png");
 
   }
 
